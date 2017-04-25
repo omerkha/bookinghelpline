2e10009cf486828d8d4f70d59075396a2ff2f064
dev <yasinadam@outlook.com>
2017-04-25 17:59:30 +0100

    1

---
+++ public/app/appRoutes.js
@@ -0,0 +1,20 @@
+app.config(function($routeProvider, $locationProvider, $httpProvider) {
+
+    var viewDir = 'app/views/';
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl : viewDir+'home/home-view.html',
+        controller  : 'HomeCtrl'
+    })
+
+  
+
+    .otherwise({
+        redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
+
+});
