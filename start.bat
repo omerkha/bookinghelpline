be193cb428b3967ee277e4fac8d7092bc44e0597
dev <yasinadam@outlook.com>
2017-05-08 13:42:48 +0100

    update

--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
-start /min cmd /k nodemon C:\Users\admin\Desktop\free\hs\index.js
+start /min cmd /k nodemon C:\Users\admin\Desktop\free\bh\index.js
 
-cd C:\Program Files (x86)\Mozilla Firefox\
-start firefox localhost:5002
 exit /b
